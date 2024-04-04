eac8c33a11063b782bf1e18fb661adb061fbe2cd
Jasurbek <krasavchikxj@gmail.com>
2024-04-04 02:06:46 +0200

    3d

---
+++ src/assets/images/index.js
@@ -0,0 +1,13 @@
+import hero from './hero.jpg'
+import meta from './meta.png'
+import starbucks from './starbucks.png'
+import tesla from './tesla.png'
+import logo from './logo.svg'
+
+export {
+    hero,
+    meta,
+    starbucks,
+    tesla,
+    logo
+}
---
+++ src/constants/index.js
@@ -0,0 +1,215 @@
+import { meta, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "React.js Developer",
+        company_name: "IT STEP Academy",
+        icon: starbucks,
+        iconBg: "#383E56",
+        date: "Jan 2022 - May 2022",
+        points: [
+            "Conducting lessons and trainings for beginning programmers",
+            "Verification and analytics of projects, code review, preparation of technical tasks.",
+            "Creating a project architecture and distributing the project according to completion and deadlines",
+            "Verification, testing, deployment and technical support",
+        ],
+    },
+    {
+        title: "Web Developer",
+        company_name: "JuniorCode",
+        icon: tesla,
+        iconBg: "#E6DEDD",
+        date: "May 2022 - Dec 2023",
+        points: [
+            "Familiarization with the technical specifications of the project, creation of a small architecture",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Full stack Developer",
+        company_name: "AIVA GROUP",
+        icon: meta,
+        iconBg: "#E6DEDD",
+        date: "Dec 2023 - Present",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/Jasurbek0424',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/jasurbek-khakimbekov-6b8ba1250/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: '4Car.kz',
+        description: '4car.kz, A company engaged in the sale of tires and wheels for cars, a multi-page site where you can make purchases online, through a bank in installments, There is also a delivery service.',
+        link: 'https://github.com/Jasurbek0424/4cars.kz-real-project',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Toounisteel.kz',
+        description: 'A company engaged in the sale and service of metal materials. Also on the website you can order a call with administrators to purchase or order goods.',
+        link: 'https://github.com/Jasurbek0424/UnisteelFront',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-blue',
+        name: 'Oryx.kz',
+        description: 'A company engaged in courier service, delivery of goods from the USA, you can also monitor the delivery status of your goods on the website, connect additional services for notification.',
+        link: 'https://github.com/Jasurbek0424/Oryx.kz',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'SafeMed',
+        description: 'SafeMed, a company that sells various devices for medicine and safety, the company also has distribution to different CIS countries.',
+        link: 'https://github.com/Jasurbek0424/safemed-real-project',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-black',
+        name: 'RentCar',
+        description: 'Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.',
+        link: 'https://github.com/Jasurbek0424/RentalCar',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'TravelGo',
+        description: 'A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.',
+        link: 'https://github.com/Jasurbek0424/Travel-go',
+    }
+];
